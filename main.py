f244d29c13626347cbd4ce230dd8316fcd670612
kandik <kandik@MacBook-Air-kandik.local>
2025-09-29 15:35:23 +0300

    def getDisciplines 1 version

--- main.py
+++ main.py
@@ -39,14 +39,14 @@ def getStudent(id):
     cur.close()
     conn.close()
 
-def getDiscipline(course_number):
+def getDiscipline(course_num):
     conn = getConn()
     if not conn:
         print(Fore.RED + "Ошибка подключения")
         return
     
     cur = conn.cursor()
-    cur.execute("SELECT name, day, pair_num FROM disciplines WHERE course_num = %s ORDER BY day, pair_num", (course_number,))
+    cur.execute("SELECT name, day, pair_num FROM disciplines WHERE course_num = %s ORDER BY day, pair_num", (course_num,))
     
     days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
     
@@ -66,13 +66,54 @@ def getDiscipline(course_number):
     cur.close()
     conn.close()
 
+def getStudents(course_num):
+    conn = getConn()
+    if not conn:
+        print(Fore.RED + "Ошибка подключения")
+        return
+    
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM students WHERE course_num = %s ORDER BY name", (course_num,))
+    
+    students = cur.fetchall()
+    
+    if students:
+        print(Fore.GREEN + f"Студенты {course_num} курса:")
+        for student in students:
+            print(f"  - {student[0]}")
+    else:
+        print(Fore.YELLOW + f"На курсе {course_num} студентов не найдено")
+    
+    cur.close()
+    conn.close()
+
+def getDisciplines():
+    conn = getConn()
+    if not conn:
+        print(Fore.RED + "Ошибка подключения")
+        return
+    
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM disciplines")
+    
+    disciplines = cur.fetchall()
+    
+    print(Fore.GREEN + "Все дисциплины:")
+    print(Fore.CYAN + "ID | Название дисциплины | День недели | Номер пары | Номер курса")
+    for disc in disciplines:
+        # Преобразуем все элементы в строки и выводим через разделитель
+        print(" | ".join(str(item) for item in disc))
+    
+    cur.close()
+    conn.close()
+
 def menu():
     for x in range(3):
         print('\n')
     print(Fore.GREEN + "   Выберите команду")
     print("1.Найти студента по id.")
     print("2.Все занятия по номеру курса.")
-    print("3.Список всех студентов.")
+    print("3.Список всех студентов по номеру курса.")
     print("4.Полное рассписание дисциплин.")
     print("5.Добавить нового студента.")
     print("6.Выйти.")
@@ -111,16 +152,23 @@ def interface():
             pressEnter()
 
         elif com == 3:
-            print("Выбрана опция 3")
-            # Код для опции 3
+            try:
+                num = int(input("Введите номер курса: "))
+                getStudents(num)
+            except ValueError:
+                print(Fore.RED + "Некоректнный ввод.")
+
+            pressEnter()
 
         elif com == 4:
-            print("Выбрана опция 4")
-            # Код для опции 4
+            getDisciplines()
+
+            pressEnter()
 
         elif com == 5:
-            print("Выбрана опция 5")
-            # Код для опции 5
+
+            
+            pressEnter()
 
 def pressEnter():
     input(Fore.CYAN + "Press enter...")
